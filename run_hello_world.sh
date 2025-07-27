6ff98c7d6bc2984604ad4c087596fc9edaa4aedc
Matan Fainzilber <fainzilber@mail.tau.ac.il>
2025-07-27 16:34:33 +0300

    removed the .sh files

--- run_hello_world.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name=hello_world
-#SBATCH --output=hello_world.out
-#SBATCH --error=hello_world.err
-#SBATCH --time=00:01:00
-#SBATCH --partition=studentkillable 
-#SBATCH --ntasks=1
-
-python3 hello_world.py
